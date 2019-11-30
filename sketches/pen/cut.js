45ec6bcd8e382ec44b436abc5afdc3071bcefad8
Charlotte Dann <ask@pouretrebelle.com>
2019-11-30 23:33:39 +0000

    :sparkles: Diagonal line grid

--- sketches/pen/cut.js
+++ sketches/pen/cut.js
@@ -1,9 +1,8 @@
-// import SimplexNoise from 'simplex-noise'
-// import Vector2 from '../../utils/Vector2'
+import SimplexNoise from 'simplex-noise'
 
-const drawPatternLines = ({ c, width, height, spacing }) => {
+const drawPatternLines = ({ c, width, height, lGap }) => {
   const minSize = Math.min(width, height)
-  const lGap = spacing * Math.sqrt(2)
+  // const lGap = spacing * Math.sqrt(2)
 
   const toRight = Array.from({ length: Math.ceil(width / lGap) }, (x, i) => ({
     x: i * lGap,
@@ -38,12 +37,41 @@ const fillPattern = (pattern, { c, x, y, width, height, ...patternProps }) => {
 }
 
 const cut = ({ c, width, height, seed }) => {
-  // const simplex1 = new SimplexNoise(seed[0])
-  // const simplex2 = new SimplexNoise(seed[1])
-  // const simplex3 = new SimplexNoise(seed[2])
-  // const simplex4 = new SimplexNoise(seed[3])
+  const simplex1 = new SimplexNoise(seed[0])
+  const simplex2 = new SimplexNoise(seed[1])
+  const simplex3 = new SimplexNoise(seed[2])
+  const simplex4 = new SimplexNoise(seed[3])
 
-  fillPattern('lines', { c, width, height, spacing: 5 })
+  const gap = 10
+  for (let y = -gap; y < height - gap; y += (height + gap) / 10) {
+    for (let x = -gap; x < width - gap; x += (width + gap) / 7) {
+      const size =
+        1 +
+        (simplex1.noise2D(x, y) > 0 ? 0 : 1) +
+        (simplex2.noise2D(x, y) > 0 ? 0 : 1) +
+        (simplex3.noise2D(x, y) > 0 ? 0 : 1) +
+        (simplex4.noise2D(x, y) > 0 ? 0 : 1)
+
+      const sizeChart = {
+        1: 1.5,
+        2: 2,
+        3: 4,
+        4: 6,
+        5: 10,
+      }
+
+      const lGap = sizeChart[size]
+
+      fillPattern('lines', {
+        c,
+        x: x + gap,
+        y: y + gap,
+        width: width / 7 - gap,
+        height: height / 10 - gap,
+        lGap,
+      })
+    }
+  }
 }
 
 export default cut

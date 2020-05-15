6fbfb5f45b1a58a82c8dcdadb468e90035d19203
Charlotte Dann <hi@cha.rs>
2020-05-15 14:35:46 +0100

    :pencil: Add title tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Helmet } from 'react-helmet'
 import GlobalStyle from 'styles/base'
 import Demo from 'components/Demo'
+import { SketchContext } from 'Provider'
 
-const App = () => (
-  <>
-    <Helmet>
-      <title>(Generative Jigsaws)</title>
-    </Helmet>
-    <GlobalStyle />
-    <Demo />
-  </>
-)
+const App = () => {
+  const [{ sketch }] = useContext(SketchContext)
+
+  return (
+    <>
+      <Helmet>
+        <title>
+          {sketch ? `${sketch.id} (Generative Jigsaws)` : 'Generative Jigsaws'}
+        </title>
+      </Helmet>
+      <GlobalStyle />
+      <Demo />
+    </>
+  )
+}
 
 export default App

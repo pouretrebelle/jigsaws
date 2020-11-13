9ec0ef2707d3ee810c57fdce75fe3cfaab2a582b
Charlotte Dann <hi@cha.rs>
2020-11-13 21:00:14 +0000

    :truck: Copy 010 to 011

---
+++ sketches/011/design/constants.ts
@@ -0,0 +1,17 @@
+import chroma from 'chroma-js'
+import { rotateHue } from 'utils/colorUtils'
+
+export const DOT_COUNT = 300
+export const MAX_LENGTH = 150
+export const LENGTH_VARIATION = 75
+export const FLOW_FIDELITY = 0.5
+export const DISTANCE_PER_FRAME = 1
+export const AVOIDANCE_THRESHOLD = 2
+export const THICKNESS = 4
+export const THICKENSS_INCREMENT = 0.05
+
+const COLORS = ['#FBEEDA', '#38FFB6', '#425BFF', '#7800A3', '#3D0016']
+export const COLOR_SCALE = chroma.scale(COLORS)
+export const BACKGROUND = chroma(rotateHue(COLOR_SCALE(0.25), 180))
+  .desaturate(0.75)
+  .hex()

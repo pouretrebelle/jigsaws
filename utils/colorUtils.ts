870867aa6e56e74ea0944df1fee5f8f53c707253
Charlotte Dann <hi@cha.rs>
2020-11-13 13:17:44 +0000

    :sparkles: Add rotateHue color helper

--- utils/colorUtils.ts
+++ utils/colorUtils.ts
@@ -1,4 +1,13 @@
+import chroma from 'chroma-js'
 import { clamp } from './numberUtils'
 
 export const hsl = (h: number, s: number, l: number): string =>
   `hsl(${h}, ${clamp(s, 0, 100)}%, ${clamp(l, 0, 100)}%)`
+
+export const rotateHue = (
+  color: string | chroma.Color,
+  rotation: number
+): string => {
+  const c = typeof color === 'string' ? chroma(color) : color
+  return c.set('hsl.h', (c.hsl()[0] + rotation) % 360).hex()
+}

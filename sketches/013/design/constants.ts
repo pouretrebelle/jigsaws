2bf9a369b5fc3860c41c778f9518ef025e8be723
Charlotte Dann <hi@cha.rs>
2020-11-15 15:38:02 +0000

    :lipstick: Tweak variables

--- sketches/013/design/constants.ts
+++ sketches/013/design/constants.ts
@@ -1,21 +1,17 @@
 import chroma from 'chroma-js'
-import { rotateHue } from 'utils/colorUtils'
 
-export const STROKE_ATTEMPTS = 500
-export const MAX_LENGTH = 150
+export const STROKE_ATTEMPTS = 200
+export const MAX_LENGTH = 200
 export const MIN_LENGTH = 30
 export const LENGTH_VARIATION = 25
-export const DOT_ATTEMPTS = 3000
-export const MIN_DOT_RADIUS = 2
-export const MAX_DOT_RADIUS = 4
-export const FLOW_FIDELITY = 0.6
+export const DOT_ATTEMPTS = 25000
+export const MIN_DOT_RADIUS = 1
+export const MAX_DOT_RADIUS = 100
+export const FLOW_FIDELITY = 2
 export const DISTANCE_PER_FRAME = 1
-export const AVOIDANCE_THRESHOLD = 1
-export const THICKNESS = 5
-export const THICKENSS_INCREMENT = 0
+export const AVOIDANCE_THRESHOLD = 3
+export const THICKNESS = 4
+export const THICKENSS_INCREMENT = 0.1
 
-const COLORS = ['#FFEADB', '#FF61C5', '#7C4CF6', '#1B22F3', '#00438F']
-export const COLOR_SCALE = chroma.scale(COLORS)
-export const BACKGROUND = chroma(rotateHue(COLOR_SCALE(1), -30))
-  .brighten(1)
-  .hex()
+export const COLOR_SCALE = chroma.cubehelix().gamma(0.5).scale()
+export const BACKGROUND = '#fff'

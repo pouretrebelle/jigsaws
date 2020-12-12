0d528375d82742be27b83b05ca1250b7d50d5adb
Charlotte Dann <hi@cha.rs>
2020-12-12 18:54:42 +0000

    :truck: Copy 013 to 014

---
+++ sketches/014/design/constants.ts
@@ -0,0 +1,11 @@
+export const STROKE_ATTEMPTS = 5000
+export const MAX_LENGTH = 150
+export const MIN_LENGTH = 20
+export const LENGTH_VARIATION = 25
+export const FLOW_FIDELITY = 1.4
+export const DISTANCE_PER_FRAME = 1
+export const AVOIDANCE_THRESHOLD = 0
+export const THICKNESS = 4
+export const THICKENSS_INCREMENT = 0
+
+export const BACKGROUND = '#3c44b5'

637f17b14af70c7f7588aa731fb9459d1b85314d
Charlotte Dann <hi@cha.rs>
2020-12-13 23:43:48 +0000

    :sparkles: Add color seed

--- sketches/016/design/constants.ts
+++ sketches/016/design/constants.ts
@@ -3,32 +3,12 @@ export const MAX_LENGTH = 150
 export const LENGTH_VARIATION = 50
 export const MIN_LENGTH = 10
 export const FLOW_FIDELITY = 0.8
-export const LAYER_SHIFT = 0.02
 export const DISTANCE_PER_FRAME = 1
 export const AVOIDANCE_THRESHOLD = 3
 export const THICKNESS = 3
 export const THICKENSS_INCREMENT = 0.1
 
 export const BACKGROUND = 'hsl(305, 40%, 80%)'
-export const LAYERS = [
-  {
-    color: 'hsl(170, 60%, 50%)',
-    composite: 'normal',
-    opacity: 1,
-  },
-  {
-    color: 'hsl(325, 100%, 60%)',
-    composite: 'normal',
-    opacity: 0.75,
-  },
-  {
-    color: 'hsl(225, 100%, 80%)',
-    composite: 'multiply',
-    opacity: 1,
-  },
-  {
-    color: 'hsl(180, 100%, 40%)',
-    composite: 'screen',
-    opacity: 1,
-  },
-]
+export const HUES = [10, 170, 180, 190, 225, 235, 275, 325, 335]
+export const LAYER_COUNT = 5
+export const LAYER_SHIFT = 0.02

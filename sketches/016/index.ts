106e668cb6e235e9a09f7487d1d753f47037bf31
Charlotte Dann <hi@cha.rs>
2020-12-13 00:55:49 +0000

    :wrench: Change piece counts

--- sketches/016/index.ts
+++ sketches/016/index.ts
@@ -7,7 +7,7 @@ export const settings = {
   width: 280,
   height: 280,
   bleed: 10,
-  rows: 15,
-  columns: 15,
+  rows: 17,
+  columns: 17,
   backgroundColor: BACKGROUND,
 } as SketchConstructorSettings

9f178a1b063b0c884069a73384b24f2b13ef04d0
Charlotte Dann <hi@cha.rs>
2021-02-04 16:21:33 +0000

    :truck: Copy 017 to 018

---
+++ sketches/018/design/constants.ts
@@ -0,0 +1,14 @@
+export const STROKE_ATTEMPTS = 200
+export const MAX_LENGTH = 150
+export const LENGTH_VARIATION = 50
+export const MIN_LENGTH = 50
+export const FLOW_FIDELITY = 0.5
+export const DISTANCE_PER_FRAME = 1
+export const AVOIDANCE_THRESHOLD = 3
+export const THICKNESS = 2
+export const THICKENSS_INCREMENT = 0.2
+
+export const BACKGROUND = 'hsl(305, 30%, 34%)'
+export const HUES = [10, 170, 180, 190, 225, 235, 275, 325, 335]
+export const LAYER_COUNT = 5
+export const LAYER_SHIFT = 0.02

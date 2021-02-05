2d3b320f38e3df525399e3aa780f5607a3c349fe
Charlotte Dann <hi@cha.rs>
2021-02-05 18:16:01 +0000

    :star: Sketch 019

--- sketches/019/design/Stroke.ts
+++ sketches/019/design/Stroke.ts
@@ -1,13 +1,10 @@
 import Vector2 from 'utils/Vector2'
 
 import {
-  AVOIDANCE_THRESHOLD,
-  DISTANCE_PER_FRAME,
-  MIN_LENGTH,
-  STROKE_THICKNESS_INCREMENT,
-  STROKE_THICKNESS,
-  LINE_WEIGHT,
+  DISTANCE_BETWEEN_RIBS,
+  RIB_WEIGHT,
   SPINE_WEIGHT,
+  RIB_WIDTH,
 } from './constants'
 
 interface StrokeConstructor {
@@ -19,14 +16,13 @@ interface StrokeConstructor {
 interface Point {
   x: number
   y: number
-  angle: number
+  angle?: number
 }
 
 const temp = new Vector2()
 
 class Stroke {
   i: number
-  STROKE_THICKNESS: number
   color: string
   pos: Vector2
   points: Point[]
@@ -36,12 +32,11 @@ class Stroke {
 
   constructor({ i, pos, color }: StrokeConstructor) {
     this.i = i
-    this.STROKE_THICKNESS = STROKE_THICKNESS
     this.color = color
     this.initialAngle = 0
 
     this.pos = pos
-    this.points = [{ x: pos.x, y: pos.y, angle: this.initialAngle }]
+    this.points = [{ x: pos.x, y: pos.y }]
     this.vel = new Vector2()
   }
 
@@ -50,7 +45,7 @@ class Stroke {
       this.initialAngle = angle
     }
 
-    this.vel.reset(DISTANCE_PER_FRAME, 0).rotate(angle)
+    this.vel.reset(DISTANCE_BETWEEN_RIBS, 0).rotate(angle)
     this.pos.plusEq(this.vel)
 
     this.points.push({
@@ -59,98 +54,30 @@ class Stroke {
       angle: this.vel.angle(),
     })
 
-    if (this.length > MIN_LENGTH) this.STROKE_THICKNESS += STROKE_THICKNESS_INCREMENT
-
-    this.length += DISTANCE_PER_FRAME
+    this.length += DISTANCE_BETWEEN_RIBS
   }
 
   draw(c: CanvasRenderingContext2D, strokes: Stroke[]) {
     c.save()
 
     c.strokeStyle = this.color
-    c.lineWidth = LINE_WEIGHT
+    c.lineWidth = RIB_WEIGHT
     c.lineCap = 'round'
 
-    this.points.slice(1, -1).forEach(({ x, y, angle }, i) => {
-      let closestDifference = Infinity
-
-      strokes.forEach((stroke) => {
-        if (stroke.i === this.i) return
-
-        stroke.points.forEach((point) => {
-          temp.reset(x, y)
-          temp.x -= point.x
-          temp.y -= point.y
-
-          closestDifference = Math.min(closestDifference, temp.magnitude())
-        })
-      })
-
-      const strokeThickness = closestDifference / 2// - AVOIDANCE_THRESHOLD / 2
-
-      temp.reset(0, strokeThickness)
-      temp.rotate(angle)
+    this.points.forEach(({ x, y, angle }, i) => {
+      temp.reset(0, RIB_WIDTH / 2)
+      temp.rotate(angle ?? this.points[1].angle as number)
 
       c.beginPath()
       c.moveTo(x + temp.x, y + temp.y)
       c.lineTo(x - temp.x, y - temp.y)
       c.stroke()
-
-      // start cap
-      if (i === 0) {
-        temp.rotate(Math.PI / 2)
-        temp.normalise()
-        const angleDiff = angle - this.points[2].angle
-        let nextPos = { x, y }
-        for (let dist = 0; dist <= strokeThickness; dist += DISTANCE_PER_FRAME) {
-          c.lineWidth = SPINE_WEIGHT
-          c.beginPath()
-
-          temp.rotate(angleDiff)
-          nextPos = { x: nextPos.x + temp.x * DISTANCE_PER_FRAME, y: nextPos.y + temp.y * DISTANCE_PER_FRAME }
-          c.moveTo(nextPos.x, nextPos.y)
-          c.lineTo(nextPos.x + temp.x * DISTANCE_PER_FRAME, nextPos.y + temp.y * DISTANCE_PER_FRAME);
-          c.stroke()
-
-          c.lineWidth = LINE_WEIGHT
-          const thisStrokeThickness = Math.sqrt(Math.pow(strokeThickness, 2) * (1 - Math.pow((dist / strokeThickness), 2)))
-          c.beginPath()
-          c.moveTo(nextPos.x + temp.y * thisStrokeThickness, nextPos.y - temp.x * thisStrokeThickness)
-          c.lineTo(nextPos.x - temp.y * thisStrokeThickness, nextPos.y + temp.x * thisStrokeThickness)
-          c.stroke()
-        }
-      }
-
-      // end cap
-      if (i === this.points.length - 3) {
-        temp.rotate(-Math.PI / 2)
-        temp.normalise()
-        const angleDiff = angle - this.points[this.points.length - 3].angle
-        let nextPos = { x, y }
-        for (let dist = 0; dist <= strokeThickness; dist += DISTANCE_PER_FRAME) {
-          c.lineWidth = SPINE_WEIGHT
-          c.beginPath()
-
-          temp.rotate(angleDiff)
-          nextPos = { x: nextPos.x + temp.x * DISTANCE_PER_FRAME, y: nextPos.y + temp.y * DISTANCE_PER_FRAME }
-          c.moveTo(nextPos.x, nextPos.y)
-          c.lineTo(nextPos.x + temp.x * DISTANCE_PER_FRAME, nextPos.y + temp.y * DISTANCE_PER_FRAME);
-          c.stroke()
-
-          c.lineWidth = LINE_WEIGHT
-          const thisStrokeThickness = Math.sqrt(Math.pow(strokeThickness, 2) * (1 - Math.pow((dist / strokeThickness), 2)))
-          c.beginPath()
-          c.moveTo(nextPos.x + temp.y * thisStrokeThickness, nextPos.y - temp.x * thisStrokeThickness)
-          c.lineTo(nextPos.x - temp.y * thisStrokeThickness, nextPos.y + temp.x * thisStrokeThickness)
-          c.stroke()
-        }
-      }
     })
 
     c.lineWidth = SPINE_WEIGHT
     c.beginPath()
     c.moveTo(this.points[0].x, this.points[0].y)
-    this.points.slice(1).forEach(({
+    this.points.forEach(({
       x, y,
     }) => c.lineTo(x, y))
     c.stroke()
--- sketches/019/design/constants.ts
+++ sketches/019/design/constants.ts
@@ -1,16 +1,13 @@
-export const STROKE_ATTEMPTS = 400
-export const MAX_LENGTH = 150
-export const LENGTH_VARIATION = 50
-export const MIN_LENGTH = 50
-export const FLOW_FIDELITY = 0.2
-export const DISTANCE_PER_FRAME = 4
-export const LINE_WEIGHT = 3
+export const SPINES_PER_LAYER = 50
+export const SPINE_LENGTH = 100
+export const FLOW_FIDELITY = 0.4
+export const SPINE_SEPARATION_FIDELITY = 0.01
+export const DISTANCE_BETWEEN_RIBS = 6
+export const RIB_WEIGHT = 4
 export const SPINE_WEIGHT = 4
-export const AVOIDANCE_THRESHOLD = 0
-export const STROKE_THICKNESS = 2
-export const STROKE_THICKNESS_INCREMENT = 0.5
-export const STROKE_OPACITY = 0.2
+export const RIB_WIDTH = 30
+export const SPINE_OPACITY = 0.3
 
-export const BACKGROUND = 'hsl(190, 40%, 40%)'
-export const HUES = [10, 170, 180, 190, 325, 335]
-export const LAYER_COUNT = 1
+export const BACKGROUND = 'hsl(225, 40%, 45%)'
+export const HUES = [10, 170, 180, 190, 225, 235, 275, 325, 335]
+export const LAYER_COUNT = 12

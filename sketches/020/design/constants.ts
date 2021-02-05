865aba33de8f85b5b26e2a4649bb34bad1c19696
Charlotte Dann <hi@cha.rs>
2021-02-05 19:19:12 +0000

    :art: Rename constants and tweak hues

--- sketches/020/design/constants.ts
+++ sketches/020/design/constants.ts
@@ -1,12 +1,21 @@
-export const SPINES_PER_LAYER = 50
-export const SPINE_LENGTH = 200
+export const STROKES_PER_LAYER = 50
+export const STROKE_LENGTH = 200
 export const FLOW_FIDELITY = 0.4
-export const SPINE_SEPARATION_FIDELITY = 0.01
-export const DISTANCE_BETWEEN_RIBS = 2
-export const RIB_WEIGHT = 1
-export const RIB_WIDTH = 5
-export const SPINE_OPACITY = 0.5
+export const STROKE_SEPARATION_FIDELITY = 0.02
+export const DISTANCE_BETWEEN_RIBS = 3
+export const RIB_WEIGHT = 3
+export const RIB_WIDTH = 20
+export const STROKE_OPACITY = 0.5
 
 export const BACKGROUND = 'hsl(225, 40%, 45%)'
-export const HUES = [10, 170, 180, 190, 225, 235, 275, 325, 335]
-export const LAYER_COUNT = 20
+export const HUES = [
+  15,
+  20,
+  75,
+  160,
+  180,
+  225,
+  320,
+  335,
+]
+export const LAYER_COUNT = 12

ca1e447c725c6602062b17f333ae2182c4db2258
Charlotte Dann <hi@cha.rs>
2021-02-05 12:07:26 +0000

    :construction: Layer spines over one another

--- sketches/019/design/constants.ts
+++ sketches/019/design/constants.ts
@@ -1,4 +1,4 @@
-export const STROKE_ATTEMPTS = 200
+export const STROKE_ATTEMPTS = 400
 export const MAX_LENGTH = 150
 export const LENGTH_VARIATION = 50
 export const MIN_LENGTH = 50
@@ -6,10 +6,10 @@ export const FLOW_FIDELITY = 0.2
 export const DISTANCE_PER_FRAME = 4
 export const LINE_WEIGHT = 3
 export const SPINE_WEIGHT = 4
-export const AVOIDANCE_THRESHOLD = 3
+export const AVOIDANCE_THRESHOLD = 0
 export const STROKE_THICKNESS = 2
 export const STROKE_THICKNESS_INCREMENT = 0.5
 
 export const BACKGROUND = 'hsl(190, 40%, 40%)'
 export const HUES = [10, 170, 180, 190, 325, 335]
-export const LAYER_COUNT = 4
+export const LAYER_COUNT = 1

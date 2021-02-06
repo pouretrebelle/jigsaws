e380410c14beb6d9fcb78229b88dafe82803fa46
Charlotte Dann <hi@cha.rs>
2021-02-06 21:57:02 +0000

    :truck: Copy 020 to 021

---
+++ sketches/021/design/constants.ts
@@ -0,0 +1,22 @@
+export const STROKES_PER_LAYER = 30
+export const STROKE_LENGTH = 200
+export const STROKE_MIN_SIZE = 5
+export const STROKE_MAX_SIZE = 30
+export const FLOW_FIDELITY = 0.4
+export const STROKE_SEPARATION_FIDELITY = 0.02
+export const DISTANCE_BETWEEN_RIBS = 5
+export const RIB_WEIGHT = 5
+export const STROKE_OPACITY = 0.5
+
+export const BACKGROUND = 'hsl(225, 40%, 45%)'
+export const HUES = [
+  15,
+  20,
+  75,
+  160,
+  180,
+  225,
+  320,
+  335,
+]
+export const LAYER_COUNT = 8

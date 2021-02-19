df47b46da106c649653ed3acacd21a86eb455920
Charlotte Dann <hi@cha.rs>
2021-02-19 16:14:57 +0000

    :lipstick: Center Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,16 +2,11 @@ import styled from 'styled-components'
 import Link from 'next/link'
 
 const StyledHeader = styled.header`
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 1em 2em;
-  align-items: center;
   margin: 2em auto;
 
   @media (min-width: 900px) {
-    grid-template-columns: 4fr 5fr;
-    padding: 0;
-    text-align: left;
+    text-align: center;
+    margin-bottom: 3em;
   }
 `
 
@@ -19,12 +14,9 @@ const StyledH1 = styled.h1`
   margin: 0;
   line-height: 1;
   font-size: 17vw;
+  margin-bottom: 0.25em;
 
-  @media (min-width: 500px) {
-    font-size: 10vw;
-  }
   @media (min-width: 900px) {
-    grid-column: 2/3;
     font-size: calc(4.6vw + 12px);
     margin-right: -1em;
   }
@@ -36,11 +28,6 @@ const StyledH1 = styled.h1`
 
 const StyledDescription = styled.aside`
   font-size: 1.5em;
-
-  @media (min-width: 900px) {
-    font-size: 1.1em;
-    grid-column: 2/3;
-  }
 
   p {
     margin: 0 0 0.25em;

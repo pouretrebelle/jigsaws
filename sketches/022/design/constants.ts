5065ac1caaba41bb5b9ba0419147b933385cb9de
Charlotte Dann <hi@cha.rs>
2021-02-25 21:51:52 +0000

    :construction: Make soft bristles

--- sketches/022/design/constants.ts
+++ sketches/022/design/constants.ts
@@ -1,13 +1,15 @@
-export const STROKES_PER_LAYER = 5
-export const STROKE_LENGTH = 200
-export const STROKE_MIN_SIZE = 20
-export const STROKE_MAX_SIZE = 100
-export const FLOW_FIDELITY = 0.2
-export const STROKE_SEPARATION_FIDELITY = 0.5
-export const DISTANCE_BETWEEN_POINTS = 0.5
-export const STROKE_OPACITY = 1
+export const STROKES_PER_LAYER = 10
+export const STROKE_LENGTH = 100
+export const STROKE_MIN_SIZE = 5
+export const STROKE_MAX_SIZE = 30
+export const FLOW_FIDELITY = 0.3
+export const STROKE_SEPARATION_FIDELITY = 0.05
+export const DISTANCE_BETWEEN_POINTS = 1
+export const STROKE_OPACITY = 0.5
+export const BRISTLE_OPACITY = 0.05
 export const BRISTLES_PER_STROKE = 20
-export const BRISTLE_WEIGHT = 2
+export const BRISTLE_MIN_WEIGHT = 2
+export const BRISTLE_MAX_WEIGHT = 10
 
 export const BACKGROUND = 'hsl(180, 40%, 45%)'
 export const HUES = [
@@ -20,5 +22,5 @@ export const HUES = [
   320,
   335,
 ]
-export const LAYER_COUNT = 3
+export const LAYER_COUNT = 8
 export const LAYER_SHIFT = 0.02

882311c87d115ed3b0d26785e7405c70b0086c8d
Charlotte Dann <hi@cha.rs>
2021-02-27 11:43:23 +0000

    :chart_with_upwards_trend: Add trackEvent function to env context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import PlausibleProvider from 'next-plausible'
+import PlausibleProvider, { usePlausible } from 'next-plausible'
 
 import GlobalStyle from 'styles/base'
 import { EnvProvider } from 'env'
@@ -8,12 +8,16 @@ import { Env } from 'types'
 
 const App = ({ Component, pageProps }: AppProps) => {
   const router = useRouter()
+  const plausible = usePlausible()
 
   return (
     <PlausibleProvider domain="abstractpuzzl.es">
       <EnvProvider
         env={process.env.NODE_ENV === 'development' ? Env.Dev : Env.Prod}
         setAppSketchId={(id) => router.push(`/app/${id}`)}
+        trackEvent={(eventName: string, props?: object) =>
+          plausible(eventName, props)
+        }
       >
         <GlobalStyle />
         <Component {...pageProps} />

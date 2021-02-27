0cacd0a8165d82a37ba0dccc5791eaf3462ae275
Charlotte Dann <hi@cha.rs>
2021-02-27 11:43:23 +0000

    :chart_with_upwards_trend: Add trackEvent function to env context

--- src/env.tsx
+++ src/env.tsx
@@ -1,19 +1,37 @@
 import { createContext } from 'react'
 import { Env } from 'types'
 
+type SetAppSketchId = (sketchId: string) => void
+type TrackEvent = (eventName: string, props?: object) => void
+
+interface Props {
+  env: Env
+  setAppSketchId: SetAppSketchId
+  trackEvent?: TrackEvent
+}
+
 interface State {
   env: Env
-  setAppSketchId: (sketchId: string) => void
+  setAppSketchId: SetAppSketchId
+  trackEvent: TrackEvent
 }
 
 export const EnvContext = createContext(({} as unknown) as State)
 
-export const EnvProvider: React.FC<State> = ({
+export const EnvProvider: React.FC<Props> = ({
   env,
   setAppSketchId,
+  trackEvent,
   children,
-}) => (
-  <EnvContext.Provider value={{ env, setAppSketchId }}>
-    {children}
-  </EnvContext.Provider>
-)
+}) => {
+  const value: State = {
+    env,
+    setAppSketchId,
+    trackEvent: (eventName, props) => {
+      if (env !== Env.Prod) console.info('%cTrack event', 'color: blue', eventName, props)
+      if (trackEvent) trackEvent(eventName, props)
+    },
+  }
+
+  return <EnvContext.Provider value={value}>{children}</EnvContext.Provider>
+}

b93186e9888a5bdff03a444b4d48f494a6339632
Charlotte Dann <hi@cha.rs>
2021-02-27 10:25:38 +0000

    :chart_with_upwards_trend: Add PlausibleProvider to App

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import type { AppProps } from 'next/app'
+import PlausibleProvider from 'next-plausible'
 
 import GlobalStyle from 'styles/base'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
+  <PlausibleProvider domain="abstractpuzzl.es">
     <GlobalStyle />
     <Component {...pageProps} />
-  </>
+  </PlausibleProvider>
 )
 
 export default App

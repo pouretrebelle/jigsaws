172061b4928b8f51d7125a515bc97798b6d45738
Charlotte Dann <hi@cha.rs>
2021-02-27 17:11:38 +0000

    :poop: Add lazy getExcerpt markdown processor

--- src/lib/markdown/processMarkdown.ts
+++ src/lib/markdown/processMarkdown.ts
@@ -3,6 +3,8 @@ import remove from 'unist-util-remove'
 import markdown from 'remark-parse'
 import remark2rehype from 'remark-rehype'
 import stringify from 'rehype-stringify'
+import toString from 'remark-stringify'
+import strip from 'strip-markdown'
 
 const stripElements: unified.Plugin<any[], { test: object }> = options => tree => {
   remove(tree, { cascade: true }, options.test)
@@ -16,4 +18,17 @@ export const processMarkdown = (string: string) => {
     .use(stringify)
 
   return processor.processSync(string).toString()
+}
+
+export const getExcerpt = (string: string) => {
+  const processor = unified()
+    .use(markdown)
+    .use(strip)
+    .use(toString)
+
+  const content = processor.processSync(string).toString()
+  const description = content.split('\n\n').slice(2)
+
+  if (description.length) return description[0].trim()
+  return ''
 }

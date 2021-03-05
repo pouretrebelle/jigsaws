20259a81687b993e57a28d8e1e19da314f2f02d3
Charlotte Dann <hi@cha.rs>
2021-03-05 14:08:04 +0000

    :sparkles: Add AppLinks to sidebar

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -1,19 +1,19 @@
 import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
-import Link from 'next/link'
 
-import { Env, EnvContext, ExceptEnv } from 'env'
+import { EnvContext } from 'env'
 import { SketchContext } from 'store/Provider'
 import { loadSketch } from 'store/actions'
 
 const H1 = styled.h1`
   margin: 0 0 0.5rem;
+  font-size: 1.5rem;
 `
 
 const Select = styled.select`
-  vertical-align: text-top;
   font-weight: bold;
   background: transparent;
+  font-size: inherit;
 
   &:focus {
     outline: none;
@@ -33,27 +33,6 @@ const Selector = () => {
   return (
     <>
       <H1>
-        <ExceptEnv env={Env.Ide}>
-          <Link href={`/${sketch?.id}`} passHref>
-            <a title="Back to site">
-              <svg
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 15 15"
-                style={{
-                  width: '1em',
-                  transform: 'rotate(0.5turn)',
-                  marginRight: '0.5em',
-                }}
-              >
-                <path
-                  d="M9.97 3.47a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H.75a.75.75 0 010-1.5h11.69L9.97 4.53a.75.75 0 010-1.06z"
-                  fill="#000"
-                />
-              </svg>
-            </a>
-          </Link>
-        </ExceptEnv>
         Sketch
         <Select
           value={sketch?.id || sketchId}
--- src/pages/app/[sketch].tsx
+++ src/pages/app/[sketch].tsx
@@ -23,7 +23,7 @@ const App: React.FC<Props> = ({ sketch, ...props }) => (
       smallImage
     />
     <SketchProvider {...props}>
-      <Demo />
+      <Demo accentColorRgb={sketch.accentColorRgb} />
     </SketchProvider>
   </>
 )

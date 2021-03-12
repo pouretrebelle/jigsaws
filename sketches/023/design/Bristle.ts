cf52c124f5a1090983f1d0223bd46e37a2b42c17
Charlotte Dann <hi@cha.rs>
2021-03-12 17:46:02 +0000

    :sparkles: Add stroke lightness variance

--- sketches/023/design/Bristle.ts
+++ sketches/023/design/Bristle.ts
@@ -3,7 +3,7 @@ import SimplexNoise from 'simplex-noise'
 import { map, randomFromNoise } from 'utils/numberUtils'
 import Vector2 from 'utils/Vector2'
 
-import { BRISTLE_MAX_WEIGHT, BRISTLE_MIN_WEIGHT, LIGHTNESS_VARIANCE } from './constants'
+import { BRISTLE_MAX_WEIGHT, BRISTLE_MIN_WEIGHT, BRISTLE_LIGHTNESS_VARIANCE, STROKE_LIGHTNESS_VARIANCE } from './constants'
 
 interface BristleConstructor {
   i: number, layerI: number, strokeI: number, bristleSimplex: SimplexNoise, hue: number, lightness: number
@@ -20,7 +20,7 @@ class Bristle {
       randomFromNoise(bristleSimplex.noise3D(layerI + 17.4, i + Math.PI, strokeI + Math.PI)) - 0.5,
     )
     this.weight = map(randomFromNoise(bristleSimplex.noise3D(layerI, strokeI, i * 20)), 0, 1, BRISTLE_MIN_WEIGHT, BRISTLE_MAX_WEIGHT)
-    this.color = `hsl(${hue}, 100%, ${lightness + bristleSimplex.noise2D(strokeI, i) * LIGHTNESS_VARIANCE}%)`
+    this.color = `hsl(${hue}, 100%, ${lightness + bristleSimplex.noise2D(strokeI, i) * BRISTLE_LIGHTNESS_VARIANCE + bristleSimplex.noise2D(layerI, strokeI) * STROKE_LIGHTNESS_VARIANCE}%)`
   }
 }
 
--- sketches/023/design/constants.ts
+++ sketches/023/design/constants.ts
@@ -6,12 +6,13 @@ export const STROKE_MAX_SIZE = 60
 export const STROKE_SIZE_VARIANCE = 0.3
 export const FLOW_FIDELITY = 0.3
 export const STROKE_SEPARATION_FIDELITY = 0.1
+export const STROKE_LIGHTNESS_VARIANCE = 30
 export const DISTANCE_BETWEEN_POINTS = 1
 export const STROKE_OPACITY = 0.2
 export const BRISTLE_OPACITY = 0.05
 export const BRISTLE_MIN_WEIGHT = 3
 export const BRISTLE_MAX_WEIGHT = 10
-export const LIGHTNESS_VARIANCE = 10
+export const BRISTLE_LIGHTNESS_VARIANCE = 10
 
 export const BACKGROUND = '#AD1F7E'
 export const HUES = [

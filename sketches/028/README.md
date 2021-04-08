5f7bc156be60166f8ecbb3daf0e29ef77826af81
Charlotte Dann <hi@cha.rs>
2021-04-08 15:31:48 +0100

    :truck: Copy 027 to 028

---
+++ sketches/028/README.md
@@ -0,0 +1,12 @@
+---
+datePublished: 2021-05-07
+youTubeLink: incoming
+
+designNoiseSeeds: []
+cutNoiseSeeds: []
+accentColor: '#50B4CC'
+
+pieces: 260
+---
+
+# 028
---
+++ sketches/028/design/constants.ts
@@ -0,0 +1,9 @@
+export const BACKGROUND = 'hsl(359, 40%, 40%)'
+
+export const GRID_ROWS = 5
+export const GRID_COLUMNS = 5
+export const LINE_COUNT = 20
+export const LINE_WEIGHT = 4
+export const LINE_CAP_SIZE = 6
+export const LINE_MAX_LENGTH = 3
+export const LINE_OPACITY = 0.6

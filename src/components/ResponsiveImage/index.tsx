68bb6d44a8e6b901905f7a98cf0dee17c7c75404
Charlotte Dann <hi@cha.rs>
2021-04-25 11:37:30 +0100

    :sparkles: Make SketchPreviewCard component to hold the seed state

---
+++ src/components/ResponsiveImage/index.tsx
@@ -0,0 +1,81 @@
+import { useState, useEffect, useRef } from 'react'
+import styled from 'styled-components'
+
+const StyledWrapper = styled.figure<{ $hasAspectRatio: boolean }>`
+  background: black;
+  ${({ $hasAspectRatio }) =>
+    $hasAspectRatio &&
+    `
+      margin: 0;
+      width: 100%;
+      height: 0;
+      overflow: hidden;
+  `}
+`
+
+const StyledImage = styled.img<{ $hasLoaded: boolean }>`
+  width: 100%;
+  opacity: ${({ $hasLoaded }) => ($hasLoaded ? 1 : 0)};
+  transition: opacity 0.1s linear;
+`
+
+interface Props {
+  formatPath: ({ width }: { width: number }) => string
+  aspectRatio?: number
+}
+
+export const ResponsiveImage: React.FC<Props> = ({
+  formatPath,
+  aspectRatio,
+  ...props
+}) => {
+  const imageWrapperElement = useRef<HTMLDivElement>(null)
+  const imageElement = useRef<HTMLImageElement>(null)
+  const [imageWidth, setImageWidth] = useState(0)
+  const [hasLoaded, setHasLoaded] = useState(false)
+
+  const calculateWidth = () => {
+    const pixelRatio = window.devicePixelRatio || 1
+    const newWidth = imageWrapperElement?.current?.clientWidth
+
+    // don't resize if it's smaller than the current
+    if (!newWidth || newWidth * pixelRatio < imageWidth) return
+
+    // round up to the nearest 50
+    setImageWidth(Math.ceil((newWidth * pixelRatio) / 50) * 50)
+  }
+
+  useEffect(() => {
+    calculateWidth()
+
+    window.addEventListener('resize', calculateWidth)
+
+    return () => {
+      window.removeEventListener('resize', calculateWidth)
+    }
+  }, [])
+
+  return (
+    <StyledWrapper
+      ref={imageWrapperElement}
+      $hasAspectRatio={!!aspectRatio}
+      style={
+        aspectRatio
+          ? {
+              paddingBottom: `${Math.round(aspectRatio * 10000) / 100}%`,
+            }
+          : {}
+      }
+    >
+      {imageWidth !== 0 && (
+        <StyledImage
+          ref={imageElement}
+          src={formatPath({ width: imageWidth })}
+          $hasLoaded={hasLoaded}
+          onLoad={() => setHasLoaded(true)}
+          {...props}
+        />
+      )}
+    </StyledWrapper>
+  )
+}
---
+++ src/components/SketchPreviewCard/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import styled from 'styled-components'
+
+import { SketchContent } from 'types'
+import { makeRandomSeed } from 'lib/seeds'
+import { ResponsiveImage } from 'components/ResponsiveImage'
+import RefreshButton from 'components/AppSidebar/Controls/RefreshButton'
+
+const StyledMeta = styled.aside`
+  display: flex;
+  min-width: 0;
+`
+
+const StyledName = styled.div`
+  flex: 1;
+  font-size: 0.75em;
+`
+
+const StyledActions = styled.div`
+  flex: 0 0 0;
+`
+
+type Props = Pick<SketchContent, 'designNoiseSeeds' | 'cutNoiseSeeds'>
+
+export const SketchPreviewCard: React.FC<Props> = ({
+  designNoiseSeeds,
+  cutNoiseSeeds,
+}) => {
+  const [designSeeds, setDesignSeeds] = useState(
+    designNoiseSeeds.map(makeRandomSeed)
+  )
+  const [cutSeeds, setCutSeeds] = useState(cutNoiseSeeds.map(makeRandomSeed))
+
+  const refreshSeeds = () => {
+    setDesignSeeds(designNoiseSeeds.map(makeRandomSeed))
+    setCutSeeds(cutNoiseSeeds.map(makeRandomSeed))
+  }
+
+  return (
+    <article>
+      <ResponsiveImage
+        formatPath={({ width }) =>
+          `/api/preview.png?width=${width}&designSeeds=${designSeeds.join(
+            ','
+          )}&cutSeeds=${cutSeeds.join(',')}`
+        }
+        aspectRatio={1}
+      />
+      <StyledMeta>
+        <StyledName>
+          {designSeeds.join('-')}_{cutSeeds.join('-')}
+        </StyledName>
+        <StyledActions>
+          <RefreshButton onClick={refreshSeeds} />
+        </StyledActions>
+      </StyledMeta>
+    </article>
+  )
+}

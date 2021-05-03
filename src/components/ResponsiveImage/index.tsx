b7aa84a59d4f7f5da1f0896fb3c6a60365c0691f
Charlotte Dann <hi@cha.rs>
2021-05-03 21:51:44 +0100

    :sparkles: Add sad face if image load times out
    Vercel has a 10s maximum execution time on hobby plans, so more complex puzzle previews may time out. This adds a cute little sad face if that happens, and improves handling of the loader timeout.
    
    See:
    - https://vercel.com/docs/platform/limits#serverless-function-execution-timeout
    - https://vercel.com/docs/platform/limits#general-limits

--- src/components/ResponsiveImage/index.tsx
+++ src/components/ResponsiveImage/index.tsx
@@ -11,7 +11,8 @@ export const spin = keyframes`
 `
 
 const StyledWrapper = styled.figure<{ $hasAspectRatio: boolean }>`
-  background: black;
+  background: #111;
+  color: rgba(255, 255, 255, 0.75);
   position: relative;
 
   ${({ $hasAspectRatio }) =>
@@ -37,12 +38,26 @@ const StyledLoader = styled.div<{ $visible: boolean }>`
   width: 2em;
   height: 2em;
   margin: -1em;
-  border: 0.125em solid rgba(255, 255, 255, 0.75);
+  border: 0.125em solid currentColor;
   border-top-color: transparent;
   border-radius: 50%;
   transition: opacity 0.2s linear;
   opacity: 0;
   animation: ${spin} 1s linear infinite;
+
+  ${({ $visible }) => $visible && `opacity: 1;`}
+`
+
+const StyledTimeout = styled.svg<{ $visible: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 2em;
+  height: 2em;
+  margin: -1em;
+  fill: currentColor;
+  transition: opacity 0.2s linear;
+  opacity: 0;
 
   ${({ $visible }) => $visible && `opacity: 1;`}
 `
@@ -61,15 +76,31 @@ export const ResponsiveImage: React.FC<Props> = ({
   const imageElement = useRef<HTMLImageElement>(null)
   const [imageWidth, setImageWidth] = useState(0)
   const [hasLoaded, setHasLoaded] = useState(false)
-  const [showLoader, setShowLoader] = useState(false)
   const loadedRef = useRef(hasLoaded)
   loadedRef.current = hasLoaded
 
+  const [showLoader, setShowLoader] = useState(false)
+  const loaderTimerRef = useRef<NodeJS.Timeout | null>(null)
+
+  const [showTimeout, setShowTimeout] = useState(false)
+  const timeoutTimerRef = useRef<NodeJS.Timeout | null>(null)
+
   useEffect(() => {
     setHasLoaded(false)
-    setTimeout(() => {
+    setShowLoader(false)
+    setShowTimeout(false)
+    if (loaderTimerRef.current) clearTimeout(loaderTimerRef.current)
+    if (timeoutTimerRef.current) clearTimeout(timeoutTimerRef.current)
+
+    loaderTimerRef.current = setTimeout(() => {
       if (!loadedRef.current) setShowLoader(true)
     }, 500)
+    timeoutTimerRef.current = setTimeout(() => {
+      if (!loadedRef.current) {
+        setShowTimeout(true)
+        setShowLoader(false)
+      }
+    }, 10000)
   }, [formatPath])
 
   const calculateWidth = () => {
@@ -96,6 +127,7 @@ export const ResponsiveImage: React.FC<Props> = ({
   const onLoad = () => {
     setHasLoaded(true)
     setShowLoader(false)
+    setShowTimeout(false)
   }
 
   return (
@@ -111,6 +143,24 @@ export const ResponsiveImage: React.FC<Props> = ({
       }
     >
       <StyledLoader $visible={showLoader} aria-hidden />
+      <StyledTimeout
+        $visible={showTimeout}
+        aria-hidden
+        xmlns="http://www.w3.org/2000/svg"
+        x="0"
+        y="0"
+        viewBox="0 0 16 16"
+      >
+        {showTimeout && (
+          <title>
+            Oh no this image couldn’t load, click here to open in explorer
+            instead
+          </title>
+        )}
+        <circle cx="5" cy="6" r="1" />
+        <path d="M11 5c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zM11.53 12.18C11.49 12.07 10.47 9.5 8 9.5c-2.48 0-3.49 2.42-3.54 2.53l-.93-.38C3.59 11.52 4.84 8.5 8 8.5c3.17 0 4.42 3.19 4.47 3.32l-.94.36z" />
+        <circle cx="11" cy="6" r="1" />
+      </StyledTimeout>
       {imageWidth !== 0 && (
         <StyledImage
           ref={imageElement}

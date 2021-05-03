9c75830348edb924c8a6e22269214217f9562382
Charlotte Dann <hi@cha.rs>
2021-05-03 17:03:39 +0100

    :bug: Fix hsla util
    The hsl notation with the alpha channel was working in the browser but not with node-canvas. I spotted this because the node-canvas outputs of 029 were much more saturated than expected.

--- utils/colorUtils.ts
+++ utils/colorUtils.ts
@@ -5,7 +5,7 @@ export const hsl = (h: number, s: number, l: number): string =>
   `hsl(${h}, ${clamp(s, 0, 100)}%, ${clamp(l, 0, 100)}%)`
 
 export const hsla = (h: number, s: number, l: number, a: number): string =>
-  `hsl(${h}, ${clamp(s, 0, 100)}%, ${clamp(l, 0, 100)}%, ${clamp(a, 0, 1)})`
+  `hsla(${h}, ${clamp(s, 0, 100)}%, ${clamp(l, 0, 100)}%, ${clamp(a, 0, 1)})`
 
 export const rotateHue = (
   color: string | chroma.Color,

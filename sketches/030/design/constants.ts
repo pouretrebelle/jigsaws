37398d26dd39d58215d63895da7a2dd4695840fe
Charlotte Dann <hi@cha.rs>
2021-05-08 22:44:47 +0100

    :fire: Remove background grid

--- sketches/030/design/constants.ts
+++ sketches/030/design/constants.ts
@@ -1,6 +1,5 @@
 export const BACKGROUND = 'hsl(212, 40%, 40%)'
 
-export const GRID_ROWS = 7
-export const GRID_COLUMNS = 7
-export const GRID_GAP_RATIO = 1/8
-export const LINE_WEIGHT = 1
+export const GRID_ROWS = 12
+export const GRID_COLUMNS = 12
+export const GRID_GAP_RATIO = 1/12

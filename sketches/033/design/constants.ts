130076af1513e2d1a4fdae99b773e40db80a97e2
Charlotte Dann <hi@cha.rs>
2021-05-12 17:28:49 +0100

    :truck: Copy 032 to 033

---
+++ sketches/033/design/constants.ts
@@ -0,0 +1,7 @@
+export const BACKGROUND = 'hsl(59, 50%, 50%)'
+
+export const GRID_ROWS = 20
+export const GRID_COLUMNS = 20
+export const GRID_GAP_RATIO = 1/2
+export const COLORS = 3
+export const LAYERS = 5

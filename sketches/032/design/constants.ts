9364c6d857880fc8c0fadcf08d6be797d958585f
Charlotte Dann <hi@cha.rs>
2021-05-12 17:27:22 +0100

    :star: 032

--- sketches/032/design/constants.ts
+++ sketches/032/design/constants.ts
@@ -1,6 +1,7 @@
-export const BACKGROUND = 'hsl(180, 40%, 30%)'
+export const BACKGROUND = 'hsl(59, 50%, 50%)'
 
-export const GRID_ROWS = 24
-export const GRID_COLUMNS = 24
-export const GRID_GAP_RATIO = 1/12
-export const LAYERS = 3
+export const GRID_ROWS = 20
+export const GRID_COLUMNS = 20
+export const GRID_GAP_RATIO = 1/2
+export const COLORS = 3
+export const LAYERS = 5

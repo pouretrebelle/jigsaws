ceac0fa6e8f31492c47f16061ec519bf1c435dec
Charlotte Dann <hi@cha.rs>
2021-05-28 21:05:01 +0100

    :sparkles: Add quadrilateral holes in cut

--- sketches/034/cut.ts
+++ sketches/034/cut.ts
@@ -1,5 +1,6 @@
 import SimplexNoise from 'simplex-noise'
 import { Cut } from 'types'
+import { randomFromNoise } from 'utils/numberUtils'
 import Vector2 from 'utils/Vector2'
 
 export enum Seeds {
@@ -7,7 +8,10 @@ export enum Seeds {
   SwayY,
   FlipX,
   FlipY,
+  Holes,
 }
+
+const HOLE_COUNT = 7
 
 const tweakDist = (
   m: number,
@@ -21,7 +25,7 @@ const tweakDist = (
     (m +
       (simplex.noise2D(m * 0.15, alt * 0.15) * 0.2 +
         simplex.noise2D(m * 0.4, alt * 0.4) * 0.1) *
-        edgeAvoidanceScalar) /
+      edgeAvoidanceScalar) /
     rows
   )
 }
@@ -59,8 +63,15 @@ const addToCurves = (
   p1: Vector2,
   p2: Vector2,
   flip: boolean,
-  moveTo: boolean
+  moveTo: boolean,
+  straight?: boolean
 ) => {
+  if (moveTo) c.moveTo(p1.x, p1.y)
+
+  if (straight) {
+    return c.lineTo(p2.x, p2.y)
+  }
+
   const tVmult = 0.25 // push of t towards other side of piece
   const tVdiv = 0.4 // push of p1c and p2c away from other side of piece
   const tWidth = 0.7 // how far t1 and t2 are from the canter
@@ -76,7 +87,6 @@ const addToCurves = (
   const t2 = t.plusNew(pV.multiplyNew(tWidth / 2))
   const p2c = p2.minusNew(pV.multiplyNew(pWidth)).minusEq(tV.multiplyNew(tVdiv))
 
-  if (moveTo) c.moveTo(p1.x, p1.y)
   c.bezierCurveTo(p1c.x, p1c.y, t1.x, t1.y, t.x, t.y)
   c.bezierCurveTo(t2.x, t2.y, p2c.x, p2c.y, p2.x, p2.y)
 }
@@ -89,6 +99,16 @@ export const cut = ({ c, width, columns, height, rows, simplex }: Cut) => {
   c.lineTo(0, height)
   c.lineTo(0, 0)
   c.stroke()
+
+  let holes: {
+    row: number
+    column: number
+  }[] = []
+  for (let i = 0; i < HOLE_COUNT; i++) {
+    const row = Math.floor(1 + randomFromNoise(simplex[Seeds.Holes].noise2D(i * 2, Math.PI)) * (rows - 2))
+    const column = Math.floor(1 + randomFromNoise(simplex[Seeds.Holes].noise2D(Math.PI, i * 2)) * (columns - 2))
+    holes.push({ row, column })
+  }
 
   const crossPoints = [] as Point[][]
 
@@ -121,7 +141,8 @@ export const cut = ({ c, width, columns, height, rows, simplex }: Cut) => {
           right,
           corner,
           simplex[Seeds.FlipX].noise2D(x * 2, y * 2) < 0,
-          true
+          true,
+          holes.some(({ row, column }) => (column === x || column - 1 === x) && row === y)
         )
       }
     }
@@ -141,7 +162,8 @@ export const cut = ({ c, width, columns, height, rows, simplex }: Cut) => {
           left,
           corner,
           simplex[Seeds.FlipY].noise2D(x * 2, y * 2) < 0,
-          true
+          true,
+          holes.some(({ row, column }) => (column === x) && (row === y || row - 1 === y))
         )
       }
     }

2c79a324831c3bb09d2d9c29bbc5106bcf4d0d60
Charlotte Dann <hi@cha.rs>
2021-05-30 22:36:47 +0100

    :truck: Copy 035 to 036

---
+++ sketches/036/design/constants.ts
@@ -0,0 +1,8 @@
+export const LAYERS = 4
+export const LINE_WIDTH = 1.5
+export const STROKE_OPACITY = 1
+export const FILL_OPACITY = 0.2
+export const GRID_ROWS = 16
+export const GRID_COLUMNS = 16
+export const GRID_FIDELITY = 0.005
+export const BACKGROUND = 'black'

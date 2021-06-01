7182639aaf1f0c82c0e3ed673c3b8b119a6f6bf1
Charlotte Dann <hi@cha.rs>
2021-06-01 22:44:19 +0100

    :truck: Copy 036 to 037

---
+++ sketches/037/README.md
@@ -0,0 +1,14 @@
+---
+datePublished: 2021-07-09
+youTubeLink: undefined
+
+designNoiseSeeds: []
+cutNoiseSeeds: []
+
+accentColor: 'fuchsia'
+
+pieces: 1
+timeToSolve: 1
+---
+
+# 037
---
+++ sketches/037/design/constants.ts
@@ -0,0 +1,6 @@
+export const LAYERS = 3
+export const FILL_OPACITY = 0.3
+export const GRID_ROWS = 4
+export const GRID_COLUMNS = 4
+export const GRID_FIDELITY = 0.005
+export const BACKGROUND = 'hsl(332, 20%, 50%)'

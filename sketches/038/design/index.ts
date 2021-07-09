3fe9e417ac552719d191642f8bebdf9820f58b05
Charlotte Dann <hi@cha.rs>
2021-07-09 15:42:49 +0100

    :construction:

--- sketches/038/design/index.ts
+++ sketches/038/design/index.ts
@@ -1,5 +1,6 @@
+import SimplexNoise from 'simplex-noise'
 import { Design } from 'types'
-import { arrayValueFromRandom } from 'utils/arrayUtils'
+import { arrayValueFromRandom, indexOfUniqueValue } from 'utils/arrayUtils'
 import { hsl } from 'utils/colorUtils'
 import { randomFromNoise } from 'utils/numberUtils'
 import {
@@ -38,6 +39,26 @@ export const design = ({
   }
   const colors = hues.map((h) => hsl(h, 50, 50))
 
+  const getColorIndex = (col: number, row: number): number =>
+    Math.floor(
+      ((simplex[Seeds.Position].noise2D(
+        col * GRID_FIDELITY_HORIZONTAL,
+        row * GRID_FIDELITY_VERTICAL
+      ) +
+        1) /
+        2) *
+        colors.length
+    )
+
+  const getColorIndexSquare = (col: number, row: number): number[] => {
+    return [
+      getColorIndex(col, row),
+      getColorIndex(col + 1, row),
+      getColorIndex(col + 1, row + 1),
+      getColorIndex(col, row + 1),
+    ]
+  }
+
   const w = width / GRID_ROWS
   const h = height / GRID_COLUMNS
   const buffer = 0.1
@@ -46,14 +67,41 @@ export const design = ({
       let x = w * col
       let y = h * row
 
-      const positionValue = simplex[Seeds.Position].noise2D(
-        col * GRID_FIDELITY_HORIZONTAL,
-        row * GRID_FIDELITY_VERTICAL
-      )
+      const colorIndexes = getColorIndexSquare(col, row)
+      const uniqueColorIndexes = [...new Set(colorIndexes)]
+      const [nw, ne, se, sw] = colorIndexes
 
-      c.fillStyle = arrayValueFromRandom(colors, (positionValue + 1) / 2)
+      const tl = { x: x - buffer, y: y - buffer }
+      const tr = { x: x + w + buffer, y: y - buffer }
+      const br = { x: x + w + buffer, y: y + h + buffer }
+      const bl = { x: x - buffer, y: y + h + buffer }
 
-      c.fillRect(x - buffer, y - buffer, w + buffer * 2, h + buffer * 2)
+      const drawTriangle = (...corners: { x: number; y: number }[]) => {
+        c.beginPath()
+        corners.forEach(({ x, y }, i) => c[i === 0 ? 'moveTo' : 'lineTo'](x, y))
+        c.fill()
+      }
+
+      if (
+        (nw === ne && sw === se) ||
+        (nw === sw && ne === se) ||
+        uniqueColorIndexes.length !== 2
+      ) {
+        c.fillStyle = colors[Math.max(...uniqueColorIndexes)]
+        c.fillRect(tl.x, tl.y, w + buffer * 2, h + buffer * 2)
+      } else if (nw !== se) {
+        c.fillStyle = colors[se]
+        drawTriangle(tr, br, bl)
+
+        c.fillStyle = colors[nw]
+        drawTriangle(tl, tr, bl)
+      } else if (ne !== sw) {
+        c.fillStyle = colors[sw]
+        drawTriangle(tl, br, bl)
+
+        c.fillStyle = colors[ne]
+        drawTriangle(tl, tr, br)
+      }
     }
   }
 

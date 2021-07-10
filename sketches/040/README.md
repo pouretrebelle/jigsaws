0c9a58b9e05ef8e025158f5ac359824b2bd58f6c
Charlotte Dann <hi@cha.rs>
2021-07-10 16:46:54 +0100

    :truck: Copy 039 to 040

---
+++ sketches/040/README.md
@@ -0,0 +1,13 @@
+---
+datePublished: 2021-07-30
+youTubeLink: todo
+
+designNoiseSeeds: []
+cutNoiseSeeds: []
+accentColor: 'fuchsia'
+
+pieces: 1
+timeToSolve: 1
+---
+
+# 039
---
+++ sketches/040/index.ts
@@ -0,0 +1,13 @@
+import { SketchConstructorSettings } from 'types'
+export { design, Seeds as DesignNoiseSeeds } from './design'
+export { cut, cutPieces, Seeds as CutNoiseSeeds } from './cut'
+
+export const settings = {
+  width: 280,
+  height: 280,
+  bleed: 10,
+  rows: 17,
+  columns: 17,
+
+  backgroundColor: 'fuchsia',
+} as SketchConstructorSettings

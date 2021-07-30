3f125ec24aa3ea4e227c6f44b9cbe8897c615582
Charlotte Dann <hi@cha.rs>
2021-07-30 17:10:09 +0100

    :truck: Copy 042 to 043

---
+++ sketches/043/index.ts
@@ -0,0 +1,13 @@
+import { SketchConstructorSettings } from 'types'
+export { design, Seeds as DesignNoiseSeeds } from './design'
+export { cut, cutPieces, Seeds as CutNoiseSeeds } from './cut'
+
+export const settings = {
+  width: 280,
+  height: 280,
+  bleed: 10,
+  rows: 16,
+  columns: 16,
+
+  backgroundColor: '#e65052',
+} as SketchConstructorSettings

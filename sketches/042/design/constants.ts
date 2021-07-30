9e896d49bce1a46e0ff49fc1d3d825c0d52d5f53
Charlotte Dann <hi@cha.rs>
2021-07-30 15:53:41 +0100

    :sparkles: Cluster circle, sharpen colour and radius contrast

--- sketches/042/design/constants.ts
+++ sketches/042/design/constants.ts
@@ -1,4 +1,5 @@
 export const COLOR_COUNT = 4
-export const CIRCLE_COUNT = 25
-export const CIRCLE_MIN_RADIUS = 10
-export const CIRCLE_MAX_RADIUS = 150
+export const CIRCLE_COUNT = 200
+export const CIRCLE_MIN_RADIUS = 20
+export const CIRCLE_MAX_RADIUS = 180
+export const CIRCLE_OPACITY_CLAMP_RADIUS = 40

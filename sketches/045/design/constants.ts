76e6ff6522de074afe5cef18d2e00312f3b7a0d8
Charlotte Dann <hi@cha.rs>
2021-08-03 19:53:14 +0100

    :truck: Copy 044 to 045

---
+++ sketches/045/design/constants.ts
@@ -0,0 +1,6 @@
+export const COLOR_COUNT = 4
+export const CIRCLE_LAYER_COUNT = 2
+export const CIRCLE_COUNT = 20
+export const CIRCLE_MIN_RADIUS = 30
+export const CIRCLE_MAX_RADIUS = 120
+export const CIRCLE_OPACITY_CLAMP_RADIUS = 60

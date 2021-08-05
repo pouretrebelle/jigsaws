0684eaf2607cb74ad62bfbc581a496691852a794
Charlotte Dann <hi@cha.rs>
2021-08-05 11:32:05 +0100

    :sparkles: Simplify design circles

--- sketches/045/design/constants.ts
+++ sketches/045/design/constants.ts
@@ -1,7 +1,7 @@
 export const COLOR_COUNT = 4
-export const CIRCLE_LAYER_COUNT = 2
-export const CIRCLE_COUNT = 20
-export const CIRCLE_MIN_RADIUS = 30
+export const CIRCLE_LAYER_COUNT = 3
+export const CIRCLE_COUNT = 10
+export const CIRCLE_MIN_RADIUS = 50
 export const CIRCLE_MAX_RADIUS = 120
 export const CIRCLE_OPACITY_CLAMP_RADIUS = 60
 export const SPIN_CIRCLE_COUNT = 15

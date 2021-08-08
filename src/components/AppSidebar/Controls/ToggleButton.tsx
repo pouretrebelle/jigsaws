229573f85d70dd7c55b0122dedb501bf0198e9ca
Charlotte Dann <hi@cha.rs>
2021-08-08 19:00:59 +0100

    :sparkles: Make toggles actual toggle buttons

--- src/components/AppSidebar/Controls/ToggleButton.tsx
+++ src/components/AppSidebar/Controls/ToggleButton.tsx
@@ -11,19 +11,51 @@ interface ButtonProps {
   active: boolean
 }
 
-const StyledToggleButton = styled.button<ButtonProps>`
-  width: 1rem;
-  height: 1rem;
-  vertical-align: text-top;
-  margin-right: 0.5rem;
-  border-radius: 50% 50%;
+const StyledToggleButton = styled.button<{
+  $active: Props['active']
+}>`
+  position: relative;
+  width: 2rem;
+  height: 1.2rem;
+  vertical-align: text-bottom;
+  margin-right: 0.6rem;
+  border-radius: 0.6em;
   cursor: pointer;
   outline: none;
-  background: ${({ active }): string => (active ? '#3ad663' : '#ccc')};
+  transition: background 0.2s;
+  background: #bbb;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 0.8em;
+    height: 0.8em;
+    background: rgba(255, 255, 255, 0.9);
+    border-radius: 50%;
+    transform: translate(-0.8em, -0.4em);
+    transition: transform 0.2s;
+  }
+
+  ${({ $active }) =>
+    $active &&
+    `
+    background: #06c777;
+    &::before {
+      transform: translate(0, -0.4em);
+    }
+  `};
 `
 
-const ToggleButton: React.FC<Props> = (props) => (
-  <StyledToggleButton {...props} />
+const ToggleButton: React.FC<Props> = ({ active, ...props }) => (
+  <StyledToggleButton
+    type="button"
+    role="switch"
+    aria-checked={active}
+    $active={active}
+    {...props}
+  />
 )
 
 export default ToggleButton
